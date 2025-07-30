22ecfd8ea26fbd38fb8b3f280ae6e7af1ff0f0a1
Apurbo Deb Nath <apurbodebnath50@gmail.com>
2025-07-30 17:23:03 +0600

    Numbers are done

--- a.py
+++ a.py
@@ -43,3 +43,14 @@ print(type(y))
 x = "John"
 # is the same as
 x = 'John'
+
+
+#                                                        Case-Sensitive
+# Variable names are case-sensitive.
+
+# Example
+# This will create two variables:
+
+a = 4
+A = "Sally"
+#A will not overwrite a
